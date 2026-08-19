a96efb1cd23e5d3e43e010a154fa62990d42e519
SaileshGoel <saileshgoel.ec25@bmsce.ac.in>
2026-08-19 16:33:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
 
-from lagrange import SystemParameters,lagrange_points,routh_stable,simulate_orbit
+from lagrange import SystemParameters,lagrange_points,routh_stable
 
 st.set_page_config(
     page_title="Lagrange Points Explorer",
